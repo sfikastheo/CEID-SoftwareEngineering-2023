957f8d8f71d1b573d20e91bb5a6cc5cf458f8a58
TriadaSimou <127792336+TriadaSimou@users.noreply.github.com>
2023-05-28 14:32:49 +0300

    Added Triada's Code
    Implementation of Creat Quiz and Job Search

---
+++ Phase-5/MariaCode/softeng2023/src/main/java/com/live_the_city/Quiz.java
@@ -0,0 +1,66 @@
+package com.live_the_city;
+
+import java.time.LocalDateTime;
+
+public class Quiz {
+
+    private int id;
+    private String title;
+    private String description;
+    private int success_points;
+    private LocalDateTime date_uploaded;
+    private Tour on_tour;
+
+    public Tour getOn_tour() {
+        return on_tour;
+    }
+    public void setOn_tour(Tour on_tour) {
+        this.on_tour = on_tour;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSuccess_points() {
+        return success_points;
+    }
+    public void setSuccess_points(int success_points) {
+        this.success_points = success_points;
+    }
+    
+    public LocalDateTime getDate_uploaded() {
+        return date_uploaded;
+    }
+    public void setDate_uploaded(LocalDateTime date_uploaded) {
+        this.date_uploaded = date_uploaded;
+    }
+
+
+    public Quiz(String title, String description, LocalDateTime upload_date) {
+    
+        this.title = title;
+        this.description = description;
+        //this.success_points = success_points;
+        this.date_uploaded = upload_date;
+    }
+
+    public Quiz() {
+    }
+}
--- Phase-5/MariaCode/softeng2023/src/main/java/com/live_the_city/ToursHistoryController.java
+++ Phase-5/MariaCode/softeng2023/src/main/java/com/live_the_city/ToursHistoryController.java
@@ -3,9 +3,14 @@ package com.live_the_city;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class ToursHistoryController {
@@ -19,12 +24,32 @@ public class ToursHistoryController {
     private Button Back_Button;
     @FXML
     private Button newTour_Button;
+    @FXML
+    private Button quiztourConnBtn;
+    @FXML
+    private TableView<Tour> toursTableView;
+
+    //-----Other variables----------
+    Quiz quiz;
+    Tour selectedTour;
+
+   
 
 
     @FXML
     void initialize() {
         //fill the table
     }
+
+    
+    @FXML 
+    void getSelectedTour(MouseEvent event) {
+        this.selectedTour = toursTableView.getSelectionModel().getSelectedItem();
+        quiztourConnBtn.setDisable(false);
+
+    } // when a tour is selected from the table
+
+
 
     @FXML
     void createTour() throws IOException{
@@ -52,5 +77,26 @@ public class ToursHistoryController {
          */
     }
 
-}
+
+
+    @FXML
+    void quizTourConnection(ActionEvent event) {
+        NewQuizController newQuizCon = new NewQuizController();
+        newQuizCon.setSelectedTour(selectedTour); //sends the selected tour to NewQuizController
+        Stage stage = (Stage) quiztourConnBtn.getScene().getWindow();
+        stage.close(); //closes current view
+    
+    }
+
+
+    //-------------------------Other functions-------------------------------
+    
+    void hide_back_newTour_Btns(){
+        Back_Button.setVisible(false);
+        newTour_Button.setVisible(false);
+        quiztourConnBtn.setVisible(true);
+    } //hides buttons when TourHistoryView is used for quiz-tour connection
+
+
+}  
 
